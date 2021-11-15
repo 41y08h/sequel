fd1b9c5ce7e26400b0eefa8853faaaf85bc4f7b0
Piyush <piyush41y08h@gmail.com>
2021-11-16 00:38:39 +0530

    fix config

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 
 const config: Knex.Config = {
   client: "postgresql",
-  connection: process.env.DATABASE_URL,
+  connection: { uri: process.env.DATABASE_URL, ssl: false },
   pool: {
     min: 2,
     max: 10,

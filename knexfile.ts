0bc8726d9da78e125eaec296f4988fdc8fd64c8a
Piyush <piyush41y08h@gmail.com>
2021-11-16 17:49:11 +0530

    fix knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -2,7 +2,12 @@ import { Knex } from "knex";
 
 const config: Knex.Config = {
   client: "pg",
-  connection: process.env.DATABASE_URL,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   pool: {
     min: 2,
     max: 10,

347476d7cf7943aad9b1ddff4f8a37011e92483a
Piyush <piyush41y08h@gmail.com>
2022-07-06 13:13:39 +0530

    commit left

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,20 +4,20 @@ import db from "../db";
 Model.knex(db);
 
 export default class User extends Model {
-  static get tableName() {
-    return "users";
-  }
+  static tableName = "users";
+  static jsonSchema = {
+    type: "object",
+    required: ["username", "password"],
+    properties: {
+      id: { type: "integer" },
+      username: { type: "string" },
+      password: { type: "string" },
+      createdAt: { type: "string" },
+    },
+  };
 
-  static get jsonSchema() {
-    return {
-      type: "object",
-      required: ["username", "password"],
-      properties: {
-        id: { type: "integer" },
-        username: { type: "string" },
-        password: { type: "string" },
-        createdAt: { type: "string", format: "date" },
-      },
-    };
-  }
+  id: number;
+  username: string;
+  password: string;
+  createdAt: string;
 }
